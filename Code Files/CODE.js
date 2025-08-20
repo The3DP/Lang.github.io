977eafbe90ea86bd8701d245d66419c7929b3e82
Bit77 <daren@markandtraci.com>
2025-08-20 14:11:47 -0500

    Update CODE.js
    Signed-off-by: Bit77 <daren@markandtraci.com>

--- Code Files/CODE.js
+++ Code Files/CODE.js
@@ -1,1 +1,18 @@
-
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, my name is The3DP");
+        System.out.println("Hola, mi nombre es The3DP");
+        System.out.println("Bonjour, je m'appelle The3DP");
+        System.out.println("Hallo, mein Name ist The3DP");
+        System.out.println("Ciao, mi chiamo The3DP");
+        System.out.println("Olá, meu nome é The3DP");
+        System.out.println("Hallo, mijn naam is The3DP");
+        System.out.println("Привет, меня зовут Дарен");
+        System.out.println("你好，我的名字是达伦");
+        System.out.println("こんにちは、私の名前はダレンです");
+        System.out.println("안녕하세요, 제 이름은 다렌입니다");
+        System.out.println("مرحباً، اسمي دارين");
+        System.out.println("नमस्ते, मेरा नाम डैरेन है");
+        System.out.println("Salve, nomen meum est The3DP");
+    }
+}
